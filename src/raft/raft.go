a6ab7f204009e7b03d84db856a6016135218637f
weijia <646025053@qq.com>
2024-07-20 17:41:02 +0800

    partA election

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -28,6 +28,27 @@ import (
 	"course/labrpc"
 )
 
+// setting the const of lower bound and upper bound of election time out
+const (
+	electionTimeoutMin time.Duration = 250 * time.Millisecond
+	electionTimeOutMax time.Duration = 400 * time.Millisecond
+)
+
+// reset the election time out
+func (rf *Raft) resetElectionTimeLocked() {
+	// obtain a random election time out
+	// plus the current time
+	rf.electionStart = time.Now()
+	randRange := int64(electionTimeOutMax - electionTimeoutMin)
+	rf.electionTimeOut = electionTimeoutMin + time.Duration(rand.Int63()%randRange)
+}
+
+// check the election time out
+func (rf *Raft) isElectionTimeOutLocked() bool {
+	// just check the election time out is out of time
+	return time.Since(rf.electionStart) > rf.electionTimeOut
+}
+
 // define Role:candidate ,follower,leader
 type Role string
 
@@ -196,17 +217,50 @@ func (rf *Raft) Snapshot(index int, snapshot []byte) {
 // field names must start with capital letters!
 type RequestVoteArgs struct {
 	// Your data here (PartA, PartB).
+	// term & candidate index
+	Term        int
+	CandidateID int //who call this rpc
 }
 
 // example RequestVote RPC reply structure.
 // field names must start with capital letters!
 type RequestVoteReply struct {
 	// Your data here (PartA).
+	Term        int
+	VoteGranted bool //whether granted to the Candidate
 }
 
 // example RequestVote RPC handler.
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	// Your code here (PartA, PartB).
+	// this function is to handle the RequestVote RPC,when receive rpc
+	// what should server do
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	reply.Term = rf.currentTerm
+	reply.VoteGranted = false
+	// if the term is lower than the current term ,reject the vote
+	if args.Term < rf.currentTerm {
+		LOG(rf.me, rf.currentTerm, DVote, "Reject vote from s%d,For T%d", args.CandidateID, args.Term)
+		return
+
+	}
+	// if the term is higher than the current term ,become follower
+	if args.Term > rf.currentTerm {
+		rf.becomeFollowerLocked(args.Term)
+	}
+	// if the server has voted for other ,reject the vote
+	if rf.votedFor != -1 && rf.votedFor != args.CandidateID {
+		LOG(rf.me, rf.currentTerm, DVote, "Reject vote from s%d,For T%d", args.CandidateID, args.Term)
+		return
+
+	}
+	// all pass ,setting reply
+	reply.VoteGranted = true
+	rf.votedFor = args.CandidateID
+	// reset clock
+	rf.resetElectionTimeLocked()
+	LOG(rf.me, rf.currentTerm, DVote, "->S%d ,vote granted", args.CandidateID)
 }
 
 // example code to send a RequestVote RPC to a server.
@@ -281,12 +335,85 @@ func (rf *Raft) killed() bool {
 	z := atomic.LoadInt32(&rf.dead)
 	return z == 1
 }
+func (rf *Raft) contextLostLocked(role Role, term int) bool {
+	return rf.role != role || rf.currentTerm != term
 
-func (rf *Raft) ticker() {
+}
+func (rf *Raft) startElection(term int) {
+	// count the vote
+	vote := 0
+	// build RPC call for each peer ,via peer &args
+	askVoteFromPeer := func(peer int, args *RequestVoteArgs) {
+		// this is client call
+		reply := &RequestVoteReply{}
+		// above code ,given  a example to call rpc,just pass arg and reply
+		// return ok
+		ok := rf.sendRequestVote(peer, args, reply)
+		if !ok {
+			// rpc failed
+			LOG(rf.me, rf.currentTerm, DDebug, "Ask vote from s%d,Lost or error", peer)
+			return
+		}
+		// if currentTerm is lower than peer,become follower
+		if reply.Term > rf.currentTerm {
+			rf.becomeFollowerLocked(reply.Term)
+			return
+		}
+		// double check current role is Candidate && term is equal
+		if rf.contextLostLocked(Candidate, term) {
+			LOG(rf.me, rf.currentTerm, DVote, "Lost content s%d,abort", peer)
+
+			return
+		}
+		// if vote granted ,vote++
+		if reply.VoteGranted {
+			vote++
+			// when half of the peer vote ,become leader
+			if vote > len(rf.peers)/2 {
+				rf.becomeLeaderLocked()
+				// start the leader work,send heartbeat & log replication
+				go rf.replicationTicker(term)
+			}
+		}
+	}
+
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	// check the term
+	if rf.contextLostLocked(Candidate, term) {
+		LOG(rf.me, rf.currentTerm, DVote, "Lost content s%d,abort", peer)
+
+		return
+		// early stop
+	}
+	// build the RPC request for each peer
+	for peer := 0; peer < len(rf.peers); peer++ {
+		if peer == rf.me {
+			// jump
+			vote++
+			continue
+		}
+		// RPC call
+		args := &RequestVoteArgs{rf.currentTerm, rf.me}
+
+	}
+}
+func (rf *Raft) electionTicker() {
 	for rf.killed() == false {
 
 		// Your code here (PartA)
 		// Check if a leader election should be started.
+
+		// here only you are not leader ,you can start election,at the same time
+		//  you should check the election time out
+		rf.mu.Lock()
+		if rf.role != Leader && rf.isElectionTimeOutLocked() {
+			rf.becomeCandidateLocked()
+			// according to the term ,send request vote to all the peers
+			// only you term is higher than the other ,you can get the vote
+			go rf.startElection(rf.currentTerm)
+		}
+		rf.mu.Unlock()
 
 		// pause for a random amount of time between 50 and 350
 		// milliseconds.
@@ -323,7 +450,7 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	rf.readPersist(persister.ReadRaftState())
 
 	// start ticker goroutine to start elections
-	go rf.ticker()
+	go rf.electionTicker()
 
 	return rf
 }
